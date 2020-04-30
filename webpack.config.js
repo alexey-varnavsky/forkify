11defe12dcf6b00cbcc639129375727539792195
Alexey Varnavsky <varnavsky84@gmail.com>
2020-04-30 13:23:46 +0300

    Updated jquery

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'production',
   entry: ['@babel/polyfill', './src/js/index.js'],
   output: {
     path: path.resolve(__dirname, 'dist'),
